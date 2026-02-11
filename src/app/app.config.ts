6d3fd791305d4b284341f55c6c1826f3324cd031
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2026-02-11 12:52:36 -0500

    Add TanStack Query for improved data fetching and state management

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideTanStackQuery, QueryClient } from '@tanstack/angular-query-experimental';
+import { withDevtools } from '@tanstack/angular-query-experimental/devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideTanStackQuery(new QueryClient(), withDevtools()),
+  ],
 };
